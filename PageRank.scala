edc7eb196fb38ae979c1729465f352c0a0e8d1f8
brijeshgautam <brijesh.bhu@gmail.com>
2016-11-06 16:06:44 +0530

    Update PageRank.scala
    updated program for writing page rank output to hdfs

--- PageRank.scala
+++ PageRank.scala
@@ -3,6 +3,10 @@ import java.io.PrintWriter
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
+import scala.collection.mutable
+import scala.util.Random
+import org.apache.hadoop.fs.Path
+
 /**
   * This  program computes  page rank of a nodes in a graph. Input to this program is file which
   * contains adjacency list representation of the graph.
@@ -12,39 +16,61 @@ import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
   * Here first entry indicates  source vertex and remaining entries are links of that node.
   */
 object PageRank {
-val epsilon =  0.0000001
+  val epsilon =  0.0000001
   def computePageRank(graph :RDD[(Int,Array[Int])]): RDD[(Int,Double)] ={
     def  computePageRankHelper(currRank:RDD[(Int, Double)], prevRank:RDD[(Int,Double)]):RDD[(Int,Double)] ={
- //  compare  prevRank and current rank value . If there is no change in the value return current rank value
-       if (currRank.join(prevRank).mapValues[Boolean]((valTuple:(Double, Double)) => valTuple._1 - valTuple._2 <= epsilon).map(idRank => idRank._2).
-         reduce((rank1, rank2) => rank1 && rank2)== true) currRank
+      //  compare  prevRank and current rank value . If there is no change in the value return current rank value
+      if (currRank.join(prevRank).mapValues[Boolean]((valTuple:(Double, Double)) => valTuple._1 - valTuple._2 <= epsilon).map(idRank => idRank._2).
+        reduce((rank1, rank2) => rank1 && rank2)== true) currRank
 
       else {
-          val contribution = graph.join(currRank).flatMap { case (source, (links, currRank)) => links.map(dest => (dest, currRank / links.size)) }
-          computePageRankHelper(contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x), currRank)
-          //ranks = contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x)
-          //println(ranks.partitioner) // identify partitioner for ranks
+        val contribution = graph.join(currRank).flatMap { case (source, (links, currRank)) => links.map(dest => (dest, currRank / links.size)) }
+        computePageRankHelper(contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x), currRank)
+        //ranks = contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x)
+        //println(ranks.partitioner) // identify partitioner for ranks
 
-        }
       }
+    }
 
-     val rank = graph.mapValues(x => 1.0)
-     val prevRank = graph.mapValues(x => 0.0)
-     //println(ranks.partitioner) // identify partitioner for ranks
-     computePageRankHelper(rank, prevRank )
+    val rank = graph.mapValues(x => 1.0)
+    val prevRank = graph.mapValues(x => 0.0)
+    //println(ranks.partitioner) // identify partitioner for ranks
+    computePageRankHelper(rank, prevRank )
   }
+  def generateGraph(sc :SparkContext, graphFile :String): Unit ={
+
+    val total = 50
+    val graph = new mutable.HashMap[Int,Set[Int]]()
+    val r = Random
+    for ( count <- 1 to total * 5){
+      val source = r.nextInt(total +1 )
+      val dest = r.nextInt(total +1)
+      graph(source) = graph.getOrElse(source, Set()) + dest
+    }
+    val hconf =sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(hconf)
+    if (fs.exists(new Path(graphFile)))
+      fs.delete(new Path(graphFile), true)
+
+    val graphRDD = sc.parallelize(graph.mapValues(_.mkString(",")).map((key) => key._1.toString + "," + key._2).toSeq)
+    graphRDD.saveAsTextFile(graphFile)
+
+  }
+
   def main (args :Array[String]): Unit ={
 
     val conf = new SparkConf().setAppName("page rank").setMaster("local")
     val sc = new SparkContext(conf)
-    val graph = sc.textFile("file:///"+args(0)).map(x => x.split(",").
+    val graphFile ="/user/inputGraph"
+    generateGraph(sc,graphFile)
+    val graph = sc.textFile("hdfs:///"+ graphFile).map(x => x.split(",").
       map(_.toInt)).map(x => (x(0),x.drop(1))).
       partitionBy(new HashPartitioner(50)).persist()
 
     val rankValue= computePageRank(graph)
-   // rankValue.collect().foreach(println)
-    rankValue.saveAsTextFile("./output")
-    //new PrintWriter("pageRankOutput.txt"){write(rankValue.collect().mkString("\n")); close}
 
+    rankValue.saveAsTextFile("hdfs:///user/root/output")
+    
   }
 }
+
