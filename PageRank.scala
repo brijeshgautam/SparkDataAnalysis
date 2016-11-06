805ee387d4b1f6885cdc43ac2020ae1782b796c4
brijeshgautam <brijesh.bhu@gmail.com>
2016-11-06 09:43:04 +0530

    Update PageRank.scala
    Update computePageRank function for using tail-recursion

--- PageRank.scala
+++ PageRank.scala
@@ -9,22 +9,29 @@ import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
   * Input entry in the file is in following format :
   * 1,6,2,4
   * 9,6,2,1,9
-  * Here first entry indicates  source vertex and remaining entries are links of that node. 
+  * Here first entry indicates  source vertex and remaining entries are links of that node.
   */
 object PageRank {
-
+val epsilon =  0.0000001
   def computePageRank(graph :RDD[(Int,Array[Int])]): RDD[(Int,Double)] ={
-    val keys = graph.map(x => x._1).max()
-    //println("keys " + keys)
+    def  computePageRankHelper(currRank:RDD[(Int, Double)], prevRank:RDD[(Int,Double)]):RDD[(Int,Double)] ={
+ //  compare  prevRank and current rank value . If there is no change in the value return current rank value
+       if (currRank.join(prevRank).mapValues[Boolean]((valTuple:(Double, Double)) => valTuple._1 - valTuple._2 <= epsilon).map(idRank => idRank._2).
+         reduce((rank1, rank2) => rank1 && rank2)== true) currRank
 
-    var ranks = graph.mapValues(x => 1.0)
+      else {
+          val contribution = graph.join(currRank).flatMap { case (source, (links, currRank)) => links.map(dest => (dest, currRank / links.size)) }
+          computePageRankHelper(contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x), currRank)
+          //ranks = contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x)
+          //println(ranks.partitioner) // identify partitioner for ranks
 
-   for ( count <- 1 to 100) {
-      val contribution = graph.join(ranks).flatMap { case (source, (links, currRank)) => links.map(dest => (dest, currRank / links.size)) }
-      ranks = contribution.reduceByKey(_ + _).mapValues(x => 0.15 + 0.85 * x)
-    }
-    ranks
+        }
+      }
 
+     val rank = graph.mapValues(x => 1.0)
+     val prevRank = graph.mapValues(x => 0.0)
+     //println(ranks.partitioner) // identify partitioner for ranks
+     computePageRankHelper(rank, prevRank )
   }
   def main (args :Array[String]): Unit ={
 
@@ -36,6 +43,7 @@ object PageRank {
 
     val rankValue= computePageRank(graph)
     rankValue.collect().foreach(println)
+
     new PrintWriter("pageRankOutput.txt"){write(rankValue.collect().mkString("\n")); close}
 
   }
